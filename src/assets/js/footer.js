2734ee757c8955cb570564daa479326422b425c9
Fabian Skutta <accounts@fabian.lol>
2023-01-08 20:15:04 +0100

    :speech_balloon: small changes

--- src/assets/js/footer.js
+++ src/assets/js/footer.js
@@ -1,4 +1,4 @@
-axios.get('https://api.github.com/repos/fabianskutta/personal-site/commits?per_page=1')
+axios.get('https://api.github.com/repos/fabianskutta/personal-site-V5/commits?per_page=1')
     .then(res => {
     let data = res.data[0];
     document.getElementById('commit').innerHTML = data.sha.substr(0, 7);
