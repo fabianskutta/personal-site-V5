76e508ddcaea7ef04cdffd96697ca2a2f4c4e4e2
Fabian Skutta <accounts@fabian.lol>
2025-06-10 23:56:46 +0200

    IOS26 update

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,16 +1,17 @@
 const menu = document.querySelector(".nav-links");
+const nav = document.querySelector("nav");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger= document.querySelector("#hamburger");
 const closeIcon= document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
+  if (nav.classList.contains("showMenu")) {
+    nav.classList.remove("showMenu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "inline";
   } else {
-    menu.classList.add("showMenu");
+    nav.classList.add("showMenu");
     closeIcon.style.display = "inline";
     menuIcon.style.display = "none";
   }
